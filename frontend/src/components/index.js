cada592aef031e50934d4d632e8563cd87af5ade
Pranit Patil <patilpranit3112@gmail.com>
2023-10-06 12:47:54 +0530

    Initial Landing Page Completed

---
+++ frontend/src/components/index.js
@@ -0,0 +1,17 @@
+import Navbar from "./Navbar";
+import Auth from "./Auth";
+import Billing from "./Billing";
+import Business from "./Business";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Login from "./Login";
+
+export {
+  Navbar,
+  Billing,
+  Business,
+  Footer,
+  Hero,
+  Auth,
+  Login
+};  
